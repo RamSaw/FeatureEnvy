cf667a12e4a2fbfda7585b8678a9b8d0ae89cea0
RamSaw <mikhail@pravilov.com>
2018-12-10 17:07:49 +0300

    Done refactoring generator. TODO: some refactorings to clean up code.

--- Algorithm/refactoringGeneration/refactoringGenerator.py
+++ Algorithm/refactoringGeneration/refactoringGenerator.py
@@ -47,7 +47,7 @@ class RefactoringGenerator:
             partiotioned_target_class_name = partition_name(opportunity[TARGET_CLASS_NAME_COLUMN])
             model_string_data = [" ".join(partitioned_method_name +
                                           partiotioned_source_class_name + partiotioned_target_class_name)]
-            # TODO: to string?
+
             test_distances = [[opportunity[SOURCE_CLASS_DISTANCE_COLUMN], opportunity[TARGET_CLASS_DISTANCE_COLUMN]]]
 
             tokenizer1 = Tokenizer(num_words=None)
@@ -77,9 +77,53 @@ class RefactoringGenerator:
                                   parameters_to_list(refactoring_to_suggest[METHOD_PARAMETERS_COLUMN]),
                                   float(refactoring_accuracy))
 
-    def get_refactoring_for_opportunities_bucket(self, refactoring_opportunities):
-        pass
+    # More correctly to use this method instead of above method because researchers in DLB in test.py use the same
+    # logic as below.
+    @staticmethod
+    def get_refactoring_for_opportunities_bucket(refactoring_opportunities):
+        partitioned_method_name = partition_name(refactoring_opportunities[0][METHOD_NAME_COLUMN])
+        partiotioned_source_class_name = partition_name(refactoring_opportunities[0][SOURCE_CLASS_NAME_COLUMN])
+        model_string_data = []
+        test_distances = []
+        for opportunity in refactoring_opportunities:
+            assert partitioned_method_name == partition_name(opportunity[METHOD_NAME_COLUMN])
+            assert partiotioned_source_class_name == partition_name(opportunity[SOURCE_CLASS_NAME_COLUMN])
+            partiotioned_target_class_name = partition_name(opportunity[TARGET_CLASS_NAME_COLUMN])
+            model_string_data.append(" ".join(partitioned_method_name +
+                                              partiotioned_source_class_name + partiotioned_target_class_name))
+            test_distances.append([opportunity[SOURCE_CLASS_DISTANCE_COLUMN],
+                                   opportunity[TARGET_CLASS_DISTANCE_COLUMN]])
+
+        tokenizer1 = Tokenizer(num_words=None)
+        tokenizer1.fit_on_texts(model_string_data)
+        test_sequences = tokenizer1.texts_to_sequences(model_string_data)
+        test_data = pad_sequences(test_sequences, maxlen=RefactoringGenerator.MAX_SEQUENCE_LENGTH)
+        test_distances = np.asarray(test_distances)
+
+        x_val = []
+        x_val_names = test_data
+        x_val_dis = test_distances
+        x_val_dis = np.expand_dims(x_val_dis, axis=2)
+        x_val.append(x_val_names)
+        x_val.append(np.array(x_val_dis))
+
+        predictions = RefactoringGenerator.model.predict_classes(x_val)
+        accuracy_predictions = RefactoringGenerator.model.predict(x_val)
+        refactoring_to_suggest = None
+        refactoring_accuracy = -1
+        index_max = list(accuracy_predictions).index(max(accuracy_predictions))
+        is_feature_envy = bool(predictions[index_max][0])
+        if is_feature_envy:
+            refactoring_to_suggest = refactoring_opportunities[index_max]
+            refactoring_accuracy = accuracy_predictions[index_max][0]
+
+        return None if refactoring_to_suggest is None else \
+            MoveMethodRefactoring(refactoring_to_suggest[TARGET_CLASS_QUALIFIED_NAME_COLUMN],
+                                  refactoring_to_suggest[SOURCE_CLASS_QUALIFIED_NAME_COLUMN],
+                                  refactoring_to_suggest[METHOD_NAME_COLUMN],
+                                  parameters_to_list(refactoring_to_suggest[METHOD_PARAMETERS_COLUMN]),
+                                  float(refactoring_accuracy))
 
     @staticmethod
     def get_refactoring(refactoring_opportunities):
-        return RefactoringGenerator.get_refactoring_for_each_opportunity(refactoring_opportunities)
+        return RefactoringGenerator.get_refactoring_for_opportunities_bucket(refactoring_opportunities)
